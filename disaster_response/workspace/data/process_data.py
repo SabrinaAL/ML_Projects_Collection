778b6d20385fab866877538e0acbcffdaddbeefe
Sabrina Aliyeva <aliyeva.sabrina@gmail.com>
2021-01-25 22:05:12 -0800

    Fixing a name for sql db

--- disaster_response/workspace/data/process_data.py
+++ disaster_response/workspace/data/process_data.py
@@ -80,7 +80,7 @@ def save_data(df, database_filename):
     database_filename (str): database name
     """
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('disaster_messages', engine, index=False)
+    df.to_sql('DisasterResponse', engine, index=False)
 
 
 def main():
