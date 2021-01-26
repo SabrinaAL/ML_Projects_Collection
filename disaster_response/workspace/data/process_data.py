5dc2dfc2def0470c589ed2cfc1901149dfa99ab5
Sabrina Aliyeva <aliyeva.sabrina@gmail.com>
2021-01-25 22:02:56 -0800

    Updating python code

--- disaster_response/workspace/data/process_data.py
+++ disaster_response/workspace/data/process_data.py
@@ -1,16 +1,86 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+      """
+      Function:
+      takes two csv files returns a merged dataFrame
+      Args:
+      messages_filepath (String): path to csv file that contains messages
+      categories_filepath (String): path to csv file that contains categories
+      Return:
+      df (DataFrame): A dataframe of messages and categories
+      """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+    # merge datasets
+    df = messages.merge(categories, on = 'id')
+    return df
 
 
 def clean_data(df):
-    pass
+    """Clean data.
+    Args:
+        df: pandas.DataFrame. dataFrame contains disaster messages and categories.
+    Return:
+        pandad.DataFrame
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df.categories
+    categories = categories.str.split(';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = list(categories.iloc[0])
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    # x = lambda row[i][0]: row[:-2] 
+    category_colnames = list(map(lambda x: x[:-2], row))
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = list(map(lambda x: x[-1], list(categories[column])))
+
+        # convert column from string to numeric
+        categories[column] = list(map(lambda x: int(x), list(categories[column])))
+
+        # Convert all value into binary (0 or 1) in related column
+        
+        if len(categories[column].value_counts()) > 2:
+            for num in list(set(categories[column].values)):
+                if num > 1:
+                    categories[column].replace([num], [0], inplace=True)
+
+    # drop the original categories column from `df`
+    df = df.drop('categories', axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Function:
+    Save the Dataframe df in a sql database
+    Args:
+    df (DataFrame): messages and categories dataframe
+    database_filename (str): database name
+    """
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('disaster_messages', engine, index=False)
 
 
 def main():
