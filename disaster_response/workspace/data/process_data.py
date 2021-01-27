74cf76ccacd3f2e361f17a4c7b7bccdd6bf32e0d
Sabrina Aliyeva <aliyeva.sabrina@gmail.com>
2021-01-27 07:03:42 -0800

    Adding if_exists='replace' in to.sql() to replace existing db

--- disaster_response/workspace/data/process_data.py
+++ disaster_response/workspace/data/process_data.py
@@ -79,7 +79,7 @@ def save_data(df, database_filename):
         database_filename (str): database name
     """
     engine = create_engine('sqlite:///{}'.format(database_filename))
-    df.to_sql('disaster_response', engine, index=False)
+    df.to_sql('disaster_response', engine, index=False, if_exists='replace')
 
 
 def main():
